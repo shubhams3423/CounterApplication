5e67370411da28dbcf868fac583517ae9c3aa993
shubhams3423 <shubhams1671h@gmail.com>
2022-11-22 12:46:00 +0530

    Added new two buttons

--- counter.js
+++ counter.js
@@ -1,18 +1,27 @@
 let zero=0;
+function plus(){
+   zero=zero+1;
+   // console.log(zero)
+   document.getElementById('zero').innerHTML = zero;
+}
+function plus1(){
+   zero=zero+2;
+   // console.log(zero)
+   document.getElementById('zero').innerHTML = zero;
+}
 function minus(){
    zero=zero-1;
    // console.log(zero)
    document.getElementById('zero').innerHTML = zero;
 }
+function minus1(){
+   zero=zero-2;
+   // console.log(zero)
+   document.getElementById('zero').innerHTML = zero;
+}
 function reset(){
    zero=0;
-   console.log('reset')
-  document.getElementById('zero').innerHTML =zero;
-
-}
-function plus()
-{
-   zero=zero+1;
+   // console.log(zero)
    document.getElementById('zero').innerHTML = zero;
 }
 
